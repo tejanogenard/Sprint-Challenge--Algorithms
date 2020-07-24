adc64f5b74a61619a90a962e7ff285bf7704775a
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-24 10:31:45 -0700

    recursive_count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,21 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+    # create an index to keep track of which letter we're on inside the word 
+    # we want to how many times we find the substring "th"
+    # increment the index and recursivelt call until we're at the end of the string
+    index = 0
+    print()
+    print(index)
+    if index < len(word):
+        print(word.count("th"))
+        print(index)
+        index = index + 1
+        print(index)
+        return count_th(word) 
+   
+
     
-    pass
+
+
+count_th("thegoththodboy")
