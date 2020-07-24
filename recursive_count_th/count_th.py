36d9a37f03d07bc0b779c0daddb31aa07f32103e
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-24 13:04:33 -0700

    count_th.pu completed

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -2,24 +2,36 @@
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
+
+P: a single word string 
+R: returning the function and returning the number of times "th" appears 
+E: 
+    input: "thegoodthboyth"
+
+        returns: 3 
+
+P:  
+    # we want to check if our word is valid length
+        if length of word < 2 
+            return 0 
+    # we want to check the index @ 0 = 't' and 1 = 'h' 
+        if length of word > 2 
+                return 1 + count_th(word[1:]) "we're updating our word length and a count"
+        else:
+            return 1 "just return one count
+    # elif index @  0 = 't' and 1 = 'h'  is false 
+        return count_th(word[1:])
+
+
 '''
 def count_th(word):
+    if len(word) < 2:
+        return 0
+
+    elif word[0:2] == 'th':
+        return 1 + count_th(word[1:])
     
-    # create an index to keep track of which letter we're on inside the word 
-    # we want to how many times we find the substring "th"
-    # increment the index and recursivelt call until we're at the end of the string
-    index = 0
-    print()
-    print(index)
-    if index < len(word):
-        print(word.count("th"))
-        print(index)
-        index = index + 1
-        print(index)
-        return count_th(word) 
-   
-
-    
+    else: 
+        return count_th(word[1:])
 
 
-count_th("thegoththodboy")
